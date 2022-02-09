0c7ef875849f36f87c1f61bd87c0c204e77f7ca5
MPurusothamNaidu-Wal <purusotham.m@westagilelabs.com>
2022-02-09 10:18:11 +0530

    commited successfully

--- src/MiniCalculator.js
+++ src/MiniCalculator.js
@@ -5,50 +5,45 @@ function MiniCalculator(props){
         return( 
         <div className="MiniCalculatorDiv">
         <h1>MiniCalculator</h1>
-        <h3>Num 1: {props.numOne}</h3>
-        <h3>Num 2 : {props.numTwo}</h3>
-        After Addition, Output is <strong>{sum}</strong>
+        <h3>Number 1: {props.numOne}</h3>
+        <h3>Number 2 : {props.numTwo}</h3>
+        Result is {sum}
         </div>)
     }
     else if(props.operation == "mul"){
-        let sum = props.numOne * props.numTwo
+        let product = props.numOne * props.numTwo
         return(
         <div className="MiniCalculatorDiv">
         <h1>MiniCalculator</h1>
-        <h3>Num 1: {props.numOne}</h3>
-        <h3>Num 2 : {props.numTwo}</h3>
-        After Multiplication, Output is <strong>{sum}</strong>
+        <h3>Number 1: {props.numOne}</h3>
+        <h3>Number 2 : {props.numTwo}</h3>
+        Result is {product}
         </div>
 
         ) 
     }
     else if(props.operation == "div"){
-        let sum = props.numOne / props.numTwo
+        let remainder = props.numOne / props.numTwo
         return(
         <div className="MiniCalculatorDiv">
         <h1>MiniCalculator</h1>
-        <h3>Num 1: {props.numOne}</h3>
-        <h3>Num 2 : {props.numTwo}</h3>
-        After Division, Output is <strong>{sum}</strong>
+        <h3>Number 1: {props.numOne}</h3>
+        <h3>Number 2 : {props.numTwo}</h3>
+        Result is {remainder}
         </div>
         ) 
     }
     else if(props.operation == "sub"){
-        let sum = props.numOne - props.numTwo
+        let num = props.numOne - props.numTwo
         return(
         <div className="MiniCalculatorDiv">
         <h1>MiniCalculator</h1>
-        <h3>Num 1: {props.numOne}</h3>
-        <h3>Num 2 : {props.numTwo}</h3>
-        After Substrattion, Output is <strong>{sum}</strong>
+        <h3>Number 1: {props.numOne}</h3>
+        <h3>Number 2 : {props.numTwo}</h3>
+        Result is {num}
         </div>
         ) 
     }
-    else{
-        return null;
-    }
-
-   
 }
 MiniCalculator.propTypes = {
     numOne: propTypes.number,
